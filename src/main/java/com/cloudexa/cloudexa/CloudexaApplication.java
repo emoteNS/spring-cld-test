45368e8511fcdea8344d8debe42f10c307f07516
emoteNS <emote@nearsoft.com>
2017-07-18 09:53:03 -0500

    .

---
+++ src/main/java/com/cloudexa/cloudexa/CloudexaApplication.java
@@ -0,0 +1,14 @@
+package com.cloudexa.cloudexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudexaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cloudexa/cloudexa/CloudexaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudexa.cloudexa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudexaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
